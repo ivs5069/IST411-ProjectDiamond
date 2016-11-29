69c451ddd58228dddd9478623f468b7b2d88518b
ivs5069 <ivs5069@gmail.com>
2016-11-28 19:10:10 -0500

    Added exception handling to file I/O. Also conditional statements to check if the user inputted a correct url. Checks if the url has or does not have 'www' and splices the string based off of that

--- cURL_Payload.py
+++ cURL_Payload.py
@@ -1,33 +1,69 @@
+'''
+	cURL_Payload.py		
+	Author: Ion Sirotkin
+	Class: IST 411
+	Last Revision Date: 11/28/2016
+	Purpose: Recieves a website inputed in by a user. Returns a JSON object of the Header information of the website
+
+'''
+
 import os, json, time
+
+def file_Error():
+	print("File Error. Make sure you have permissions to write to this folder or have disk space to write to.")
+	exit()
 
 #Get the website that the user wants to use
 #website = input("Please input what website you want header information from: ")
 website = "https://github.com/" #Default value for testing so I don't have to keep putting in a website
 
-web_Name = website[website.index('/') + 2: website.index('.'):]  #Get the name of the website
+#If the url conatains www
+if("www" in website):
+	web_Name = website[website.index('.') + 1 : website[website.index('.') + 1: :].index('.') + website.index('.') + 1:] #Splice the string between the period after www and before the extension
+
+#If the url contains http or https splice the string between the second '/' character and before the extension
+elif("http" in website):
+	web_Name = website[website.index('/') + 2: website.index('.'):]  #Get the name of the website
+
+#Else statement, splices from the beginning to before the extension
+else:
+	print("Please try again and input a website. Do not forget to input 'http://' or 'https://'")
+	exit()
+
 
 #Create the string for the cURL command.Saves the cURL output to the websites name with file extention .curl"
 website_inp = "curl -I " + website + "> " + web_Name + "_curl.txt"
 
 #run the command
-os.system(website_inp)
+try:
+	os.system(website_inp)
+except:
+	file_Error()	
 
 #Create an empty dictionary
 new_Dict = {}
 
 #Open the .curl file and iterate through it
-with open(web_Name + "_curl.txt") as f:
-	for line in f:
-		#If the line contains the : character, add it to the dictionary.
-		if(':' in line):
-			new_Dict[line[0:line.index(':'):]] = line[line.index(':') + 2::]
+try:
+	with open(web_Name + "_curl.txt") as f:
+		for line in f:
+			#If the line contains the : character, add it to the dictionary.
+			if(':' in line):
+				new_Dict[line[0:line.index(':'):]] = line[line.index(':') + 2::]
+except:
+	file_Error()
 
 #Add the date/time and subsystem diamond information
+new_Dict["Website Name"] = web_Name
 new_Dict["Diamond Time"] = time.time()
 new_Dict["Diamond System"] = "cURL Payload"
 
 #Create a new file to hold the json file, write to the file the Json file, and then close the file
-f = open(web_Name + ".json", 'w')
+try:
+	f = open(web_Name + ".json", 'w')
+except:
+	file_Error
+
 f.write(json.dumps(new_Dict))
 f.close()
 
